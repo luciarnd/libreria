7e0aeda14c8582ef7d1c2aab77a060f9c90eb512
larnedo01 <larnedo@hiberus.com>
2022-03-25 14:27:57 +0100

    Perfecionando Funciones

--- src/main/java/com/example/Ejercicio/back/resource/CategoriaResource.java
+++ src/main/java/com/example/Ejercicio/back/resource/CategoriaResource.java
@@ -1,9 +1,13 @@
 package com.example.Ejercicio.back.resource;
 
+import com.example.Ejercicio.back.dto.LibroDTO;
 import com.example.Ejercicio.back.model.Categoria;
+import com.example.Ejercicio.back.model.Libro;
 import com.example.Ejercicio.back.services.CategoriaServices;
+import com.example.Ejercicio.back.services.LibroServices;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,9 +16,13 @@ import java.util.List;
 @RequestMapping("/categoria")
 public class CategoriaResource{
     private final CategoriaServices categoriaServices;
+    private final LibroServices libroServices;
+    private final LibroResource libroResource;
 
-    public CategoriaResource(CategoriaServices categoriaServices) {
+    public CategoriaResource(CategoriaServices categoriaServices, LibroServices libroServices, LibroResource libroResource) {
         this.categoriaServices = categoriaServices;
+        this.libroServices=libroServices;
+        this.libroResource=libroResource;
     }
 
     @GetMapping("/all")
@@ -39,6 +47,23 @@ public class CategoriaResource{
     }
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Categoria> deleteCategoria(@PathVariable("id") Long id){
+        List<Libro> libros = libroServices.findAllLibros();
+        for(int i=0; i < libros.size(); i++) {
+            LibroDTO dto = new LibroDTO();
+            dto.setId(libros.get(i).getId());
+            dto.setTitulo(libros.get(i).getTitulo());
+            dto.setEdicion(libros.get(i).getEdicion());
+            if(libros.get(i).getAutor() != null) {
+                dto.setAutor(libros.get(i).getAutor().getNombre() + " " + libros.get(i).getAutor().getApellido1());
+            }
+            dto.setCategoria(libros.get(i).getCategoria().getDescripcion());
+
+            if(dto.getCategoria().equals(categoriaServices.findCategoriaById(id).getDescripcion())) {
+                dto.setCategoria("Sin categoria");
+                libroResource.updateLibro(dto);
+
+            }
+        }
         categoriaServices.deleteCategoria(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
